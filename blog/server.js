dcede72d5f10ecca5484064948df615bbe0badaf
DarylHolman <dholmanj@gmail.com>
2017-06-25 16:18:38 -0400

    Day Late Dollar Short

--- blog/server.js
+++ blog/server.js
@@ -1,30 +1,40 @@
-ar express = require("express"),
-	bodyParser = require("body-parser"),
-	mongoose = require("mongoose"),
-	bcrypt = require("bcrypt-nodejs")
-	hbs = require("hbs"),
-	app = express(),
-	id = require("uuid/v4"),
-	User = require("./user");
-	path = require("path");
+var express = require("express"),
+    bodyParser = require("body-parser"),
+    hbs = require("hbs"),
+    path = require("path"),
+    session = require("express-session"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    methodOverride = require("method-override"),
+    auth = require("./app/auth/passport-local"),
+    routes = require("./app/routes/routes"),
+    app = express();
 
-app.use("/static", path.join(__dirname, "app/client"));
+
+app.use("/static", express.static(path.join(__dirname, "app/client")));
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(session({
+    secret: "itsAMeMario",
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(methodOverride('_method'));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set("views", path.join(__dirname, "app/views"));
 app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "app/client"));
 
+auth(passport);
+routes(app, passport);
 
-}};
-
-app.use("/style.css"());
-
-
-
-	}
-}
-
-mongoose.connect("mongodb://localhost/api")
+mongoose.connect("mongodb://localhost/blog");
 app.listen(8080);
-
-console.log("server is running")
+console.log("Blog is Running");
